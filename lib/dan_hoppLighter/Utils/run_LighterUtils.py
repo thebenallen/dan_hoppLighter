19e4e991e9a0470c4d8317559007f5a6c599f30e
Dan Hopp <exnehilo7@gmail.com>
2025-02-25 15:25:05 -0500

    Update Utils. Add to UI files.

--- lib/dan_hoppLighter/Utils/run_LighterUtils.py
+++ lib/dan_hoppLighter/Utils/run_LighterUtils.py
@@ -2,17 +2,39 @@ import subprocess
 import os
 
 # This will contain functions to run Lighter command line tool
-def run_lighter(input_file, output_file, output_dir, kmer_size, genome_size, threads):
+def run_lighter(input_files, output_file, output_dir, kmer_params, kmer_length, genome_size, alpha=None, threads=10, maxcor=4, trim=False, discard=False, noQual=False, newQual=None, saveTrustedKmers=None, loadTrustedKmers=None, zlib=None):
     try:
         # Construct the Lighter command with additional parameters
         command = [
             'lighter',
-            '-r', input_file,
             '-od', output_dir,
-            '-K', str(kmer_size),
-            str(genome_size),
-            '-t', str(threads)
+            '-t', str(threads),
+            '-maxcor', str(maxcor)
         ]
+        
+        # Add multiple input files
+        for input_file in input_files:
+            command.extend(['-r', input_file])
+        
+        if kmer_params == '-k':
+            command.extend([kmer_params, str(kmer_length), str(genome_size), str(alpha)])
+        elif kmer_params == '-K':
+            command.extend([kmer_params, str(kmer_length), str(genome_size)])
+        
+        if trim:
+            command.append('-trim')
+        if discard:
+            command.append('-discard')
+        if noQual:
+            command.append('-noQual')
+        if newQual is not None:
+            command.extend(['-newQual', str(newQual)])
+        if saveTrustedKmers is not None:
+            command.extend(['-saveTrustedKmers', saveTrustedKmers])
+        if loadTrustedKmers is not None:
+            command.extend(['-loadTrustedKmers', loadTrustedKmers])
+        if zlib is not None:
+            command.extend(['-zlib', str(zlib)])
         
         # Run the Lighter command and capture the output
         result = subprocess.run(command, capture_output=True, text=True, check=True)
@@ -25,13 +47,15 @@ def run_lighter(input_file, output_file, output_dir, kmer_size, genome_size, thr
             
         print(f"Lighter command executed successfully. Output saved to {output_file}\n")
 
-        # Get the file name's prefix to the left of the . from input_file. Ignore the path.
-        prefix = input_file.split('/')[-1].split('.')[0]
+        # Get the file name's prefixes to the left of the . from the input files. Ignore the path.
+        prefixes = [input_file.split('/')[-1].split('.')[0] for input_file in input_files]
 
         # Return in a dictionary two file names found in the output_dir folder: The first has a .html prefix and the second has '.cor.' in the name and its filename prefix is the same as the input file's prefix.
+        corrected_files = [output_dir + '/' + f for f in os.listdir(output_dir) if any(prefix in f for prefix in prefixes)]
+
         return {
             'console_output_file': output_file,
-            'corrected_file': output_dir + '/' + [f for f in os.listdir(output_dir) if prefix in f][0]
+            'corrected_files': corrected_files
         }
 
     except subprocess.CalledProcessError as e:
